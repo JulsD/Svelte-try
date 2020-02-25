d9311242d4409a13bb20caae23393097c439728a
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2020-02-25 17:29:03 +0100

    svelte: add clicking example

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
+<script>
+	let count = 0;
+
+    $: ending = count === 1 ? '' : 's';
+    
+    function increaseCount() {
+        return count++
+    }
+</script>
+
 <h1>Hello world!</h1>
+<button on:click={increaseCount}>Click to increase the count</button>
+<p>Count: {count} time{ending}</p>
