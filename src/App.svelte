b21ce3b71ac672857082afa9669b4254b933be52
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2020-02-26 11:21:17 +0100

    svelte: add default props value sample

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,11 @@
         return count++
     }
 
-    export let answer;
+    export let answer = 'lost in woods', propWithDefault = 'mistery';
 </script>
 
 <h1>Hello world!</h1>
 <button on:click={increaseCount}>Click to increase the count</button>
 <p>Count: {count} time{ending}</p>
-<p>Prop passed from the parent is {answer}</p>
+<p>Prop passed from the parent is <strong>{answer}</strong></p>
+<p>As well we have some prop which's default name is <strong>{propWithDefault}</strong> and it can be overwriten by parent props</p>
