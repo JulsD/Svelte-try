dc963108cad1f930d8e3708d7900df1185ba2a09
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2020-02-26 11:35:17 +0100

    svelte: add conditions sample

--- src/App.svelte
+++ src/App.svelte
@@ -14,4 +14,10 @@
 <button on:click={increaseCount}>Click to increase the count</button>
 <p>Count: {count} time{ending}</p>
 <p>Prop passed from the parent is <strong>{answer}</strong></p>
+{#if answer == '42'}
+<p>-- it is The Answer to the Ultimate Question of Life, The Universe, and Everything.</p>
+{/if}
+{#if answer !== 42}
+<p>-- never know what can be here</p>
+{/if}
 <p>As well we have some prop which's default name is <strong>{propWithDefault}</strong> and it can be overwriten by parent props</p>
